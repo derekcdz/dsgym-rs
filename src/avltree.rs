8f5f1a9a6bdf18458d5016e2e2f00c8b431dbaf2
derekcdz <derekcodez@gmail.com>
2021-10-26 20:50:29 +0800

    find

--- src/avltree.rs
+++ src/avltree.rs
@@ -35,15 +35,15 @@ impl<T: PartialOrd + Display> AVLTree<T> {
         }
     }
 
-    pub fn add_inner(root: &mut Link<T>, key: T) -> Link<T> {
+    pub fn add_inner(mut root: Link<T>, key: T) -> Link<T> {
         match root.take() {
             Some(node) => {
                 {
-                    let mut x = node.as_ref().borrow_mut();
+                    let mut x = node.borrow_mut();
                     if key.lt(&x.key) {
-                        x.left = Self::add_inner(&mut x.left.take(), key)
+                        x.left = Self::add_inner(x.left.take(), key)
                     } else if x.key.lt(&key) {
-                        x.right = Self::add_inner(&mut x.right.take(), key)
+                        x.right = Self::add_inner(x.right.take(), key)
                     }
                 }
                 Some(node)
@@ -54,34 +54,51 @@ impl<T: PartialOrd + Display> AVLTree<T> {
         }
     }
 
-    pub fn rotate_left(root: &mut Link<T>) -> Link<T> {
+    pub fn rotate_left(mut root: Link<T>) -> Link<T> {
         root.take().map(|x| {
             let mut rch = x.borrow_mut().right.take().unwrap();
-            let mut rchlch = rch.borrow_mut().left.take().unwrap();
-            x.borrow_mut().right = Some(rchlch);
+            let mut rchlch = rch.borrow_mut().left.take();
+            x.borrow_mut().right = rchlch;
             rch.borrow_mut().left = Some(x);
             rch
         })
     }
 
-    pub fn rotate_right(root: &mut Link<T>) -> Link<T> {
+    pub fn rotate_right(mut root: Link<T>) -> Link<T> {
         root.take().map(|x| {
             let mut lch = x.borrow_mut().left.take().unwrap();
-            let mut lchrch = lch.borrow_mut().right.take().unwrap();
-            x.borrow_mut().left = Some(lchrch);
+            let mut lchrch = lch.borrow_mut().right.take();
+            x.borrow_mut().left = lchrch;
             lch.borrow_mut().right = Some(x);
             lch
         })
     }
 
     pub fn add(&mut self, key: T) {
-        self.root = Self::add_inner(&mut self.root.take(), key)
+        self.root = Self::add_inner(self.root.take(), key)
+    }
+
+    fn find(root: &Link<T>, key: T) -> Link<T> {
+        root.as_ref().and_then(|node| {
+            let x = node.borrow();
+            if key.lt(&x.key) {
+                Self::find(&x.left, key)
+            } else if x.key.lt(&key) {
+                Self::find(&x.right, key)
+            } else {
+                Some(node.clone())
+            }
+        })
+    }
+
+    pub fn contains(&self, key: T) -> bool {
+        Self::find(&self.root, key).is_some()
     }
 
     pub fn traverse_inner(node: &Link<T>) {
         node.as_ref().map(|x| {
             Self::traverse_inner(&x.as_ref().borrow().left);
-            println!("=>{}", x.as_ref().borrow().key);
+            println!("=>{}", x.borrow().key);
             Self::traverse_inner(&x.as_ref().borrow().right);
         });
     }
@@ -104,6 +121,12 @@ mod test {
         t.add(3);
         t.add(2);
         t.add(4);
-        t.traverse();
+        assert_eq!(t.contains(1), true);
+        assert_eq!(t.contains(2), true);
+        assert_eq!(t.contains(3), true);
+        assert_eq!(t.contains(4), true);
+        assert_eq!(t.contains(5), true);
+        assert_eq!(t.contains(6), false);
+        assert_eq!(t.contains(0), false);
     }
 }
